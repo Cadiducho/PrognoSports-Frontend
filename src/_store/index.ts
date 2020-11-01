6fe09d4df9225139db6d52376fd32c35d4745238
Cadiducho <cadiducho@gmail.com>
2020-11-01 13:07:57 +0100

    Eliminar codigo de vuex no suado

--- src/_store/index.ts
+++ src/_store/index.ts
@@ -2,8 +2,6 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import {IAuthState} from "@/_store/modules/AuthModule";
 import {ICommunityState} from "@/_store/modules/CommunityModule";
-import {IDriverState} from "@/_store/modules/DriversModule";
-import {IGPsState} from "@/_store/modules/GrandPrixesModule";
 import {IUserState} from "@/_store/modules/UserModule";
 
 Vue.use(Vuex);
@@ -13,8 +11,6 @@ const debug = process.env.NODE_ENV !== 'production';
 export interface IRootState {
   auth: IAuthState,
   community: ICommunityState,
-  drivers: IDriverState,
-  grandprix: IGPsState,
   user: IUserState,
 }
 
