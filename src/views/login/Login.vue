f9a0d74aa7a90e32deb028e65d109b77eab09ddb
= <contacto@albertodiazsaez.com>
2020-11-28 02:17:14 +0100

    feat(ISSUE-6): Quitado codigo comentado, centrado formulario de login y ajustado tamanyo

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -2,62 +2,71 @@
   <div id="loginComponent">
     <LandingNavbar />
     <div class="container">
-      <div class="card">
-        <div class="card-header">
-          <div class="card-header-title">Datos de inicio de sesión</div>
-        </div>
-        <div class="card-content">
-          <form>
-            <div class="field">
-              <label class="label">Usuario</label>
-              <div class="control has-icons-left has-icons-right">
-                <input
-                  v-model="username"
-                  id="username"
-                  autofocus
-                  required
-                  class="input"
-                  :class="{ 'is-danger': submitted && !username }"
-                  type="text"
-                  name="username"
-                />
-                <span class="icon is-small is-left">
-                  <i class="fas fa-user"></i>
-                </span>
+      <div class="columns is-centered">
+        <div class="column is-two-fifths">
+          <div class="card">
+            <div class="card-header">
+              <div class="card-header-title">Datos de inicio de sesión</div>
+            </div>
+            <div class="card-content">
+              <form>
+                <div class="field">
+                  <label class="label">Usuario</label>
+                  <div class="control has-icons-left has-icons-right">
+                    <input
+                      v-model="username"
+                      id="username"
+                      autofocus
+                      required
+                      class="input"
+                      :class="{ 'is-danger': submitted && !username }"
+                      type="text"
+                      name="username"
+                    />
+                    <span class="icon is-small is-left">
+                      <i class="fas fa-user"></i>
+                    </span>
+                  </div>
+                </div>
+                <div class="field">
+                  <label class="label">Contraseña</label>
+                  <div class="control has-icons-left has-icons-right">
+                    <input
+                      v-model="password"
+                      id="password"
+                      required
+                      class="input"
+                      :class="{ 'is-danger': submitted && !password }"
+                      type="password"
+                      name="password"
+                    />
+                    <span class="icon is-small is-left">
+                      <i class="fas fa-lock"></i>
+                    </span>
+                  </div>
+                </div>
+                <div class="field is-grouped">
+                  <div class="control">
+                    <button
+                      v-on:click.prevent="handleSubmit()"
+                      class="button is-link"
+                    >
+                      Acceder
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div class="card-footer">
+              <div class="card-footer-item">
+                <router-link to="/register">Registrarse</router-link>
+              </div>
+              <div class="card-footer-item">
+                <router-link to="/forgotpassword"
+                  >He olvidado mi contraseña</router-link
+                >
               </div>
             </div>
-            <div class="field">
-              <label class="label">Contraseña</label>
-              <div class="control has-icons-left has-icons-right">
-                <input
-                  v-model="password"
-                  id="password"
-                  required
-                  class="input"
-                  :class="{ 'is-danger': submitted && !password }"
-                  type="password"
-                  name="password"
-                />
-                <span class="icon is-small is-left">
-                  <i class="fas fa-lock"></i>
-                </span>
-              </div>
-            </div>
-            <div class="field is-grouped">
-              <div class="control">
-                <button v-on:click.prevent="handleSubmit()" class="button is-link">Acceder</button>
-              </div>
-            </div>
-          </form>
-        </div>
-        <div class="card-footer">
-          <div class="card-footer-item">
-            <router-link to="/register">Registrarse</router-link>
-          </div>
-          <div class="card-footer-item">
-            <router-link to="/forgotpassword"
-              >He olvidado mi contraseña</router-link
-            >
           </div>
         </div>
       </div>
@@ -65,41 +74,12 @@
   </div>
 </template>
 
-<!-- <form @submit.prevent="handleSubmit" aria-label="Login">
-                            <div class="form-group row">
-                                <label for="username" class="col-sm-4 col-form-label text-md-right">Usuario</label>
-
-                                <div class="col-md-6">
-                                    <input v-model="username" id="username" type="text" class="form-control" :class="{ 'is-invalid': submitted && !username }" name="username" value required autofocus/>
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label for="password" class="col-md-4 col-form-label text-md-right">Contraseña</label>
-
-                                <div class="col-md-6">
-                                    <input v-model="password" id="password" type="password" class="form-control" :class="{ 'is-invalid': submitted && !password }" name="password" required />
-                                </div>
-                            </div>
-                            <div class="form-group row mb-0">
-                                <div class="col-md-8 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">Acceder</button>
-                                </div>
-                            </div>
-                            <h6>
-                                <router-link to="/forgotpassword">He olvidado mi contraseña</router-link>
-                            </h6>
-                            <h6>
-                                <router-link to="/register">Registrarse</router-link>
-                            </h6>
-                        </form> -->
-
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import { AuthModule } from "@/_store/modules/AuthModule";
 import { UserModule } from "@/_store/modules/UserModule";
 import LandingNavbar from "@/components/landing/LandingNavbar.vue";
-import { Action } from 'vuex-module-decorators';
+import { Action } from "vuex-module-decorators";
 
 @Component({
   components: { LandingNavbar },
@@ -115,7 +95,6 @@ export default class Login extends Vue {
     }
   }
 
-  @Action
   public handleSubmit() {
     this.submitted = true;
     if (this.username && this.password) {
@@ -129,8 +108,7 @@ export default class Login extends Vue {
             type: "is-success",
           });
 
-          // this.$router.push({ name: "home" });
-
+          this.$router.push({ name: "home" });
         },
         (error: any) => {
           this.$buefy.toast.open({
