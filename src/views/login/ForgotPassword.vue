d11013276264438829623986b736169b1deb236a
= <contacto@albertodiazsaez.com>
2020-11-28 17:34:00 +0100

    feat(ISSUE-6): Cambiado el TAB size de 2 a 4

--- src/views/login/ForgotPassword.vue
+++ src/views/login/ForgotPassword.vue
@@ -1,68 +1,82 @@
 <template>
-  <div id="forgotPasswordComponent">
-    <LandingNavbar />
-    <div class="container">
-      <div class="columns is-centered">
-        <div class="column is-two-fifths">
-          <div class="card">
-            <div class="card-header">
-              <div class="card-header-title">Cambio de contraseña</div>
-            </div>
-            <div class="card-content">
-              <div class="content">
-                <p class="has-text-justified">
-                  Para solicitar un cambio de contraseña, deberás introducir el
-                  correo electrónico de tu cuenta, donde recibirás un código de
-                  recuperación.
-                </p>
-                <p class="has-text-justified">
-                  Con dicho código podrás establecer una nueva contraseña para
-                  tu cuenta.
-                </p>
-              </div>
-              <form>
-                <div class="field">
-                  <label class="label">Correo electrónico</label>
-                  <div class="control has-icons-left has-icons-right">
-                    <input
-                      v-model="email"
-                      id="email"
-                      required
-                      class="input"
-                      :class="{ 'is-danger': submitted && !email }"
-                      type="email"
-                      name="email"
-                    />
-                    <span class="icon is-small is-left">
-                      <i class="fas fa-at"></i>
-                    </span>
-                  </div>
+    <div id="forgotPasswordComponent">
+        <LandingNavbar />
+        <div class="container">
+            <div class="columns is-centered">
+                <div class="column is-two-fifths">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="card-header-title">
+                                Cambio de contraseña
+                            </div>
+                        </div>
+                        <div class="card-content">
+                            <div class="content">
+                                <p class="has-text-justified">
+                                    Para solicitar un cambio de contraseña,
+                                    deberás introducir el correo electrónico de
+                                    tu cuenta, donde recibirás un código de
+                                    recuperación.
+                                </p>
+                                <p class="has-text-justified">
+                                    Con dicho código podrás establecer una nueva
+                                    contraseña para tu cuenta.
+                                </p>
+                            </div>
+                            <form>
+                                <div class="field">
+                                    <label class="label"
+                                        >Correo electrónico</label
+                                    >
+                                    <div
+                                        class="control has-icons-left has-icons-right"
+                                    >
+                                        <input
+                                            v-model="email"
+                                            id="email"
+                                            required
+                                            class="input"
+                                            :class="{
+                                                'is-danger':
+                                                    submitted && !email,
+                                            }"
+                                            type="email"
+                                            name="email"
+                                        />
+                                        <span class="icon is-small is-left">
+                                            <i class="fas fa-at"></i>
+                                        </span>
+                                    </div>
+                                </div>
+                                <div class="field is-grouped">
+                                    <div class="control">
+                                        <button
+                                            v-on:click.prevent="handleSubmit()"
+                                            class="button is-link"
+                                        >
+                                            Solicitar
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="card-footer">
+                            <div class="card-footer-item">
+                                <router-link to="/register"
+                                    >Registrarse</router-link
+                                >
+                            </div>
+                            <div class="card-footer-item">
+                                <router-link to="/login"
+                                    >Ya tengo usuario</router-link
+                                >
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="field is-grouped">
-                  <div class="control">
-                    <button
-                      v-on:click.prevent="handleSubmit()"
-                      class="button is-link"
-                    >
-                      Solicitar
-                    </button>
-                  </div>
-                </div>
-              </form>
             </div>
-            <div class="card-footer">
-              <div class="card-footer-item">
-                <router-link to="/register">Registrarse</router-link>
-              </div>
-              <div class="card-footer-item">
-                <router-link to="/login">Ya tengo usuario</router-link>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script lang="ts">
@@ -71,91 +85,101 @@ import { UserModule } from "@/_store/modules/UserModule";
 import LandingNavbar from "@/components/landing/LandingNavbar.vue";
 
 @Component({
-  components: { LandingNavbar },
+    components: { LandingNavbar },
 })
 export default class ForgotPassword extends Vue {
-  private email: string = "";
-  private inputToken: string = "";
-  private inputPassword: string = "";
-  private showChangePassword: boolean = false;
+    private email: string = "";
+    private inputToken: string = "";
+    private inputPassword: string = "";
+    private showChangePassword: boolean = false;
 
-  handleSubmit(e: any) {
-    if (!this.showChangePassword) {
-      const { email } = this;
-      if (email) {
-        UserModule.userForgotPassword(email).then(
-          () => {
-            this.$buefy.toast.open({
-              message: "Tu código de verificación ha sido enviado",
-              type: "is-success",
-            });
-            this.showChangePassword = true;
-          },
-          (error) => {
-            if (error === "User email cannot be null") {
-              this.$buefy.toast.open({
-                duration: 5000,
-                message: "Debes introducir tu dirección de email",
-                type: "is-danger",
-              });
-            } else if (error === "User not found") {
-              this.$buefy.toast.open({
-                duration: 5000,
-                message: "Usuario no encontrado",
-                type: "is-danger",
-              });
+    handleSubmit(e: any) {
+        if (!this.showChangePassword) {
+            const { email } = this;
+            if (email) {
+                UserModule.userForgotPassword(email).then(
+                    () => {
+                        this.$buefy.toast.open({
+                            message:
+                                "Tu código de verificación ha sido enviado",
+                            type: "is-success",
+                        });
+                        this.showChangePassword = true;
+                    },
+                    (error) => {
+                        if (error === "User email cannot be null") {
+                            this.$buefy.toast.open({
+                                duration: 5000,
+                                message:
+                                    "Debes introducir tu dirección de email",
+                                type: "is-danger",
+                            });
+                        } else if (error === "User not found") {
+                            this.$buefy.toast.open({
+                                duration: 5000,
+                                message: "Usuario no encontrado",
+                                type: "is-danger",
+                            });
+                        }
+                    }
+                );
             }
-          }
-        );
-      }
-    } else {
-      const { email, inputToken, inputPassword } = this;
-      if (email) {
-        UserModule.userChangePassword(email, inputToken, inputPassword).then(
-          () => {
-            this.$buefy.toast.open({
-              message: "Tu contraseña ha sido restablecida",
-              type: "is-success",
-            });
-            this.$router.push("/login");
-          },
-          (error) => {
-            if (error === "User email cannot be null") {
-              this.$buefy.toast.open({
-                duration: 5000,
-                message: "Debes introducir tu dirección de email",
-                type: "is-danger",
-              });
-            } else if (error === "User not found") {
-              this.$buefy.toast.open({
-                duration: 5000,
-                message: "Usuario no encontrado",
-                type: "is-danger",
-              });
-            } else if (error === "You must send the security token") {
-              this.$buefy.toast.open({
-                duration: 5000,
-                message: "Debes escribir el código de seguridad recibido",
-                type: "is-danger",
-              });
-            } else if (error === "You must send new the password") {
-              this.$buefy.toast.open({
-                duration: 5000,
-                message: "Debes escribir tu nueva contraseña",
-                type: "is-danger",
-              });
-            } else if (error === "Token rejected") {
-              this.$buefy.toast.open({
-                duration: 5000,
-                message:
-                  "Token rechazado. Compruebalo bien o vuelve a intentarlo en 15 minutos",
-                type: "is-danger",
-              });
+        } else {
+            const { email, inputToken, inputPassword } = this;
+            if (email) {
+                UserModule.userChangePassword(
+                    email,
+                    inputToken,
+                    inputPassword
+                ).then(
+                    () => {
+                        this.$buefy.toast.open({
+                            message: "Tu contraseña ha sido restablecida",
+                            type: "is-success",
+                        });
+                        this.$router.push("/login");
+                    },
+                    (error) => {
+                        if (error === "User email cannot be null") {
+                            this.$buefy.toast.open({
+                                duration: 5000,
+                                message:
+                                    "Debes introducir tu dirección de email",
+                                type: "is-danger",
+                            });
+                        } else if (error === "User not found") {
+                            this.$buefy.toast.open({
+                                duration: 5000,
+                                message: "Usuario no encontrado",
+                                type: "is-danger",
+                            });
+                        } else if (
+                            error === "You must send the security token"
+                        ) {
+                            this.$buefy.toast.open({
+                                duration: 5000,
+                                message:
+                                    "Debes escribir el código de seguridad recibido",
+                                type: "is-danger",
+                            });
+                        } else if (error === "You must send new the password") {
+                            this.$buefy.toast.open({
+                                duration: 5000,
+                                message: "Debes escribir tu nueva contraseña",
+                                type: "is-danger",
+                            });
+                        } else if (error === "Token rejected") {
+                            this.$buefy.toast.open({
+                                duration: 5000,
+                                message:
+                                    "Token rechazado. Compruebalo bien o vuelve a intentarlo en 15 minutos",
+                                type: "is-danger",
+                            });
+                        }
+                    }
+                );
             }
-          }
-        );
-      }
+        }
     }
-  }
 }
 </script>
--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,77 +1,91 @@
 <template>
-  <div id="loginComponent">
-    <LandingNavbar />
-    <div class="container">
-      <div class="columns is-centered">
-        <div class="column is-two-fifths">
-          <div class="card">
-            <div class="card-header">
-              <div class="card-header-title">Datos de inicio de sesión</div>
-            </div>
-            <div class="card-content">
-              <form>
-                <div class="field">
-                  <label class="label">Usuario</label>
-                  <div class="control has-icons-left has-icons-right">
-                    <input
-                      v-model="username"
-                      id="username"
-                      autofocus
-                      required
-                      class="input"
-                      :class="{ 'is-danger': submitted && !username }"
-                      type="text"
-                      name="username"
-                    />
-                    <span class="icon is-small is-left">
-                      <i class="fas fa-user"></i>
-                    </span>
-                  </div>
+    <div id="loginComponent">
+        <LandingNavbar />
+        <div class="container">
+            <div class="columns is-centered">
+                <div class="column is-two-fifths">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="card-header-title">
+                                Datos de inicio de sesión
+                            </div>
+                        </div>
+                        <div class="card-content">
+                            <form>
+                                <div class="field">
+                                    <label class="label">Usuario</label>
+                                    <div
+                                        class="control has-icons-left has-icons-right"
+                                    >
+                                        <input
+                                            v-model="username"
+                                            id="username"
+                                            autofocus
+                                            required
+                                            class="input"
+                                            :class="{
+                                                'is-danger':
+                                                    submitted && !username,
+                                            }"
+                                            type="text"
+                                            name="username"
+                                        />
+                                        <span class="icon is-small is-left">
+                                            <i class="fas fa-user"></i>
+                                        </span>
+                                    </div>
+                                </div>
+                                <div class="field">
+                                    <label class="label">Contraseña</label>
+                                    <div
+                                        class="control has-icons-left has-icons-right"
+                                    >
+                                        <input
+                                            v-model="password"
+                                            id="password"
+                                            required
+                                            class="input"
+                                            :class="{
+                                                'is-danger':
+                                                    submitted && !password,
+                                            }"
+                                            type="password"
+                                            name="password"
+                                        />
+                                        <span class="icon is-small is-left">
+                                            <i class="fas fa-lock"></i>
+                                        </span>
+                                    </div>
+                                </div>
+                                <div class="field is-grouped">
+                                    <div class="control">
+                                        <button
+                                            v-on:click.prevent="handleSubmit()"
+                                            class="button is-link"
+                                        >
+                                            Acceder
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="card-footer">
+                            <div class="card-footer-item">
+                                <router-link to="/register"
+                                    >Registrarse</router-link
+                                >
+                            </div>
+                            <div class="card-footer-item">
+                                <router-link to="/forgotpassword"
+                                    >He olvidado mi contraseña</router-link
+                                >
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="field">
-                  <label class="label">Contraseña</label>
-                  <div class="control has-icons-left has-icons-right">
-                    <input
-                      v-model="password"
-                      id="password"
-                      required
-                      class="input"
-                      :class="{ 'is-danger': submitted && !password }"
-                      type="password"
-                      name="password"
-                    />
-                    <span class="icon is-small is-left">
-                      <i class="fas fa-lock"></i>
-                    </span>
-                  </div>
-                </div>
-                <div class="field is-grouped">
-                  <div class="control">
-                    <button
-                      v-on:click.prevent="handleSubmit()"
-                      class="button is-link"
-                    >
-                      Acceder
-                    </button>
-                  </div>
-                </div>
-              </form>
             </div>
-            <div class="card-footer">
-              <div class="card-footer-item">
-                <router-link to="/register">Registrarse</router-link>
-              </div>
-              <div class="card-footer-item">
-                <router-link to="/forgotpassword"
-                  >He olvidado mi contraseña</router-link
-                >
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script lang="ts">
@@ -82,43 +96,43 @@ import LandingNavbar from "@/components/landing/LandingNavbar.vue";
 import { Action } from "vuex-module-decorators";
 
 @Component({
-  components: { LandingNavbar },
+    components: { LandingNavbar },
 })
 export default class Login extends Vue {
-  username: string = "";
-  password: string = "";
-  submitted: boolean = false;
+    username: string = "";
+    password: string = "";
+    submitted: boolean = false;
 
-  created() {
-    if (!!UserModule.registeredMail) {
-      this.username = UserModule.registeredMail;
-    }
-  }
-
-  public handleSubmit() {
-    this.submitted = true;
-    if (this.username && this.password) {
-      AuthModule.authRequest({
-        username: this.username,
-        password: this.password,
-      }).then(
-        () => {
-          this.$buefy.toast.open({
-            message: "¡Has iniciado sesión correctamente!",
-            type: "is-success",
-          });
-
-          this.$router.push({ name: "home" });
-        },
-        (error: any) => {
-          this.$buefy.toast.open({
-            duration: 5000,
-            message: "Fallo al iniciar sesión: " + error,
-            type: "is-danger",
-          });
+    created() {
+        if (!!UserModule.registeredMail) {
+            this.username = UserModule.registeredMail;
         }
-      );
     }
-  }
+
+    public handleSubmit() {
+        this.submitted = true;
+        if (this.username && this.password) {
+            AuthModule.authRequest({
+                username: this.username,
+                password: this.password,
+            }).then(
+                () => {
+                    this.$buefy.toast.open({
+                        message: "¡Has iniciado sesión correctamente!",
+                        type: "is-success",
+                    });
+
+                    this.$router.push({ name: "home" });
+                },
+                (error: any) => {
+                    this.$buefy.toast.open({
+                        duration: 5000,
+                        message: "Fallo al iniciar sesión: " + error,
+                        type: "is-danger",
+                    });
+                }
+            );
+        }
+    }
 }
 </script>
