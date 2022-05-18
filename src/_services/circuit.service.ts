2158fefa083d87c1ce8f7199b784a1824d5ab54d
Cadiducho <cadiducho@gmail.com>
2022-05-18 16:04:53 +0200

    Circuit service

--- src/_services/circuit.service.ts
+++ src/_services/circuit.service.ts
@@ -1,23 +1,39 @@
 import axios from "axios";
-import { Circuit } from '@/types/Circuit';
-import { CircuitVariant } from "@/types/CircuitVariant";
+import {Circuit, ICircuit} from '@/types/Circuit';
+import {CircuitVariant, ICircuitVariant} from "@/types/CircuitVariant";
+import {PrognoService} from "@/_services/progno.service";
+import {GrandPrix, IGrandPrix} from "@/types/GrandPrix";
 
-export class CircuitService {
+export class CircuitService extends PrognoService<ICircuit, Circuit>{
+
+
+    factory(data: ICircuit): Circuit {
+        return new Circuit(data);
+    }
 
     public async getCircuitList() : Promise<Array<Circuit>> {
-        return await axios.get(`/circuits`);
+        return this.getObjectListFromAPI(`/circuits`);
     }
 
     public async getCircuit(circuitId: string) : Promise<Circuit> {
-        return await axios.get(`/circuits/${circuitId}`);
+        return this.getObjectFromAPI(`/circuits/${circuitId}`);
     }
 
     public async getCircuitWithVariant(circuitId: string, variantId: string) : Promise<Circuit> {
-        return await axios.get(`/circuits/${circuitId}/${variantId}`);
+        return this.getObjectFromAPI(`/circuits/${circuitId}/${variantId}`);
     }
 
     public async listCircuitVariant(circuit: Circuit): Promise<Array<CircuitVariant>> {
-        return await axios.get(`/circuits/${circuit.id}/variants`);
+        const variants: Array<CircuitVariant> = [];
+        // @ts-ignore
+        let promise: Promise<ICircuitVariant[]> = await axios.get<ICircuitVariant[]>(`/circuits/${circuit.id}/variants`);
+        promise.then((data => {
+            data.forEach(variant => {
+                variants.push(new CircuitVariant(variant));
+            })
+        }));
+
+        return variants;
     }
 
     public async createCircuit(data: any): Promise<Circuit> {
