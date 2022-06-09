48bc90c3500bb01b90a33b8a4045a2bc98bca27d
Cadiducho <cadiducho@gmail.com>
2022-06-09 16:07:05 +0200

    Fix Breadcrumb

--- src/components/lib/Breadcrumb.vue
+++ src/components/lib/Breadcrumb.vue
@@ -13,49 +13,51 @@
 </template>
 
 <script lang="ts">
-import {RouteRecordPublic} from "vue-router";
-import {defineComponent} from "vue";
+import {RouteParams, RouteRecord, useRoute, useRouter} from "vue-router";
+import {defineComponent, onMounted, ref} from "vue";
+import useEmitter from "@/composables/useEmitter";
 
-type Dictionary<T> = { [key: string]: T }
-interface BreadcrumbRoute extends RouteRecordPublic {
-    parent: RouteRecordPublic | undefined;
-    params: Dictionary<string>;
+interface BreadcrumbRoute extends RouteRecord {
+    parent: RouteRecord | undefined;
+    params: RouteParams;
 }
 
 interface Crumb {
     title: string;
     isLast: boolean;
     target: string;
-    params: Dictionary<string>;
+    params: RouteParams;
 }
 
 export default defineComponent({
-    data() {
-        return {
-            crumbs: [] as Array<Crumb>
-        }
-    },
-    methods: {
+    setup() {
+        const emitter = useEmitter();
+        const router = useRouter();
+        const route = useRoute();
+
+        let crumbs = ref([] as Array<Crumb>);
+
         /**
          * Calcular a partir de la ruta actual del componente su Breadcrumb
          */
-        calculateCrumbs(): Array<Crumb> {
+        const calculateCrumbs = () => {
+            console.log("crumbs")
             let list: Crumb[] = [];
 
-            const myRoute = this.$router.getRoutes().find(r => r.name == this.$route.name) as BreadcrumbRoute;
-            myRoute.params = this.$route.params;
+            const myRoute = router.getRoutes().find(r => r.name == route.name) as BreadcrumbRoute;
+            myRoute.params = route.params;
 
-            list.push(...this.findParents(myRoute).reverse());
+            list.push(...findParents(myRoute).reverse());
 
             return list;
-        },
+        }
 
         /**
          * Buscar recursivamente a los padres de la ruta y añadir aquellos que tengan un titulo a la lista de breadcrumb
          * @param route La ruta sobre la que buscar sus ancestros
          * @param parents Rutas ya agregadas, si existen
          */
-        findParents(route: BreadcrumbRoute, parents: Crumb[] = []): Array<Crumb> {
+        const findParents = (route: BreadcrumbRoute, parents: Crumb[] = []) => {
             if (route.meta!.title) {
 
                 let crumb: Crumb = {
@@ -68,11 +70,25 @@ export default defineComponent({
             }
 
             if (route.parent != undefined) {
-                this.findParents(route.parent as BreadcrumbRoute, parents);
+                findParents(route.parent as BreadcrumbRoute, parents);
             }
 
             return parents;
         }
-    }
+
+        onMounted(() => {
+            crumbs.value = calculateCrumbs();
+            // Recibir eventos de otros componentes para sobreescribir el nombre del crumb actual
+            // Por ejemplo, "Detalles del Circuito" puede ser sustitido por "Autodromo Nazionale di Monza"
+            emitter.on('breadcrumbLastname', (name: string) => {
+                crumbs.value.forEach(crumb => {
+                    if (crumb.isLast) {
+                        crumb.title = name;
+                    }
+                })
+            });
+        });
+        return {emitter, crumbs}
+    },
 });
 </script>
