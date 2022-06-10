cee9a5fe8d2b67a8d70f0b4b530bd92fdec524f0
Cadiducho <cadiducho@gmail.com>
2022-06-10 18:12:06 +0200

    Rediseñar listado de gps

--- src/composables/useDayjs.ts
+++ src/composables/useDayjs.ts
@@ -4,6 +4,7 @@ import timezone from "dayjs/plugin/timezone";
 import relativeTime from "dayjs/plugin/relativeTime";
 import customParseFormat from "dayjs/plugin/customParseFormat";
 import isToday from "dayjs/plugin/isToday";
+import weekOfYear from "dayjs/plugin/weekOfYear"
 import 'dayjs/locale/es';
 
 export function useDayjs() {
@@ -14,12 +15,19 @@ export function useDayjs() {
     dayjs.extend(relativeTime);
     dayjs.extend(customParseFormat);
     dayjs.extend(isToday);
+    dayjs.extend(weekOfYear);
 
     const dateDiff = (date: Date): string => {
         return dayjs(date).fromNow();
     }
     const isBefore = (date: Date): boolean => {
         return dayjs().isBefore(date);
+    }
+    const isAfter = (date: Date): boolean => {
+        return dayjs().isAfter(date);
+    }
+    const isThisWeek = (date: Date): boolean => {
+        return dayjs().week() == dayjs(date).week();
     }
     const humanDate = (date: Date, timezone: string = "Europe/Madrid"): string => {
         return dayjs.tz(date, timezone).format('D/M/YYYY');
@@ -30,6 +38,12 @@ export function useDayjs() {
     const humanDateTimeMinusFiveMinutes = (date: Date, timezone: string = "Europe/Madrid"): string => {
         return dayjs.tz(date, timezone).subtract(5, 'minute').format('D/M/YY [a las] H:mm');
     }
+    const humanMonth = (date: Date, timezone: string = "Europe/Madrid"): string => {
+        return dayjs.tz(date, timezone).format('MMMM');
+    }
+    const humanDayMonth = (date: Date, timezone: string = "Europe/Madrid"): string => {
+        return dayjs.tz(date, timezone).format('D [de] MMMM');
+    }
 
-    return { dateDiff, isBefore, humanDate, humanDateTime, humanDateTimeMinusFiveMinutes };
+    return { dateDiff, isBefore, isAfter, isThisWeek, humanDate, humanDateTime, humanDateTimeMinusFiveMinutes, humanMonth, humanDayMonth };
 }
