f8f8961d36a4c9b71aea4d6f39c0e2763e01a3f9
Cadiducho <cadiducho@gmail.com>
2022-07-22 21:59:27 +0200

    Rework of UserResume

--- src/components/user/UserLevelResume.vue
+++ src/components/user/UserLevelResume.vue
@@ -3,11 +3,11 @@
         <div class="level-item has-text-centered">
             <o-tooltip multilined variant="light">
                 <template v-slot:content>
-                    {{ userResume.standing }}º en el ranking de esta comunidad
+                    {{ userResume.standings }}º en el ranking de esta comunidad
                 </template>
 
                 <p class="heading">Puesto</p>
-                <p class="title">{{ userResume.standing || '?'}}º</p>
+                <p class="title">{{ userResume.standings || '0'}}º</p>
             </o-tooltip>
         </div>
         <div class="level-item has-text-centered">
@@ -15,40 +15,67 @@
                 <o-tooltip multilined>
                     <template v-slot:content>
                         <ul>
-                            <li>
-                                <b>Carreras</b>: {{ userResume.pointsInRaces }}
-                            </li>
-                            <li>
-                                <b>Clasificaciones</b>: {{ userResume.pointsInQualis }}
+                            <li v-for="(points, session) in userResume.totalPointsBySession">
+                                <b>{{ session }}</b>: {{ points }}
                             </li>
                         </ul>
                     </template>
 
                     <p class="heading">Puntos</p>
                     <p class="title">
-                        {{ userResume.points || '?' }}
+                        {{ userResume.totalPoints || '0' }}
                     </p>
                 </o-tooltip>
             </div>
         </div>
         <div class="level-item has-text-centered">
-            <o-tooltip multilined variant="light">
+            <div>
+                <o-tooltip multilined>
+                    <template v-slot:content>
+                        <ul>
+                            <li v-for="(points, session) in userResume.averagePointsBySession">
+                                <b>{{ session }}</b>: {{ points }}
+                            </li>
+                        </ul>
+                    </template>
+
+                    <p class="heading">Media Puntos por sesión</p>
+                    <p class="title">
+                        {{ userResume.average || '0' }}
+                    </p>
+                </o-tooltip>
+            </div>
+        </div>
+        <div class="level-item has-text-centered">
+            <o-tooltip multilined>
                 <template v-slot:content>
-                    Esta funcionalidad aún no está implementada
+                    <ul>
+                        <li v-for="(gps, session) in userResume.wonSessions">
+                            <b>{{ session }}</b>: {{ gps.join(', ') }}
+                        </li>
+                    </ul>
                 </template>
 
                 <p class="heading">Sesiones ganadas</p>
-                <p class="title">?</p>
+                <p class="title">
+                    {{ countWonSessions(this.userResume.wonSessions) || '0' }}
+                </p>
             </o-tooltip>
         </div>
         <div class="level-item has-text-centered">
-            <o-tooltip multilined variant="light">
+            <o-tooltip multilined>
                 <template v-slot:content>
-                    Esta funcionalidad aún no está implementada
+                    <ul>
+                        <li v-for="(name) in userResume.wonGrandPrixes">
+                            {{ name }}
+                        </li>
+                    </ul>
                 </template>
 
                 <p class="heading">Grandes Premios ganados</p>
-                <p class="title">?</p>
+                <p class="title">
+                    {{ countWonGPs(this.userResume.wonGrandPrixes) || '0' }}
+                </p>
             </o-tooltip>
         </div>
     </nav>
@@ -59,6 +86,10 @@
 import {UserResume} from "@/types/User";
 
 import {defineComponent, PropType} from "vue";
+import {isValidCommunity} from "@/utils";
+import {userService} from "@/_services";
+import {useAuthStore} from "@/store/authStore";
+import {useCommunityStore} from "@/store/communityStore";
 
 export default defineComponent({
     name: "UserLevelResume",
@@ -66,7 +97,49 @@ export default defineComponent({
         userResume: {
             type: Object as PropType<UserResume>
         }
-    }
+    },
+    setup() {
+        const authStore = useAuthStore();
+        const communityStore = useCommunityStore();
 
+        const currentUser = authStore.user;
+        const currentCommunity = communityStore.community;
+        return { currentUser, currentCommunity };
+    },
+    data() {
+        return {
+            userResume: {} as UserResume,
+        }
+    },
+    mounted() {
+        if (isValidCommunity(this.currentCommunity)) {
+            const competition = this.currentCommunity.competition;
+            const season = competition.currentSeason;
+
+            userService.getUserResume(this.currentUser, this.currentCommunity, competition, season).then((resume) => {
+                this.userResume = resume;
+            });
+        }
+    },
+    methods: {
+        countWonSessions(wonSessions: Map<string, string[]>) {
+            let count = 0;
+            if (wonSessions) {
+                Object.values(wonSessions).forEach((list, ses) => {
+                    count += list.length;
+                });
+            }
+
+            return count;
+        },
+        countWonGPs(wonGrandPrixes: string[]) {
+            let count = 0;
+            if (wonGrandPrixes) {
+                count += wonGrandPrixes.length;
+            }
+
+            return count;
+        }
+    }
 });
 </script>
