10c53b7cdc04dff6201ba17110b1d29a575ded69
Chicle <chicle@protonmail.com>
2023-03-30 19:11:46 +0100

    Test object removed

--- src/components/home/PointsAccumulated.vue
+++ src/components/home/PointsAccumulated.vue
@@ -52,14 +52,6 @@ export default defineComponent({
     },
     data() {
         return {
-            chartOptionss: {
-                chart: {
-                id: "vuechart-example",
-                },
-                xaxis: {
-                categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
-                },
-            },
             series: [
                 {
                 name: "series-1",
