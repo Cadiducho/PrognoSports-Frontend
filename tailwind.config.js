6a41db6dc4bb762b7384939edee4b53d7992047a
Cadiducho <cadiducho@gmail.com>
2023-07-14 17:28:39 +0200

    init darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     './src/**/*.{vue,js,ts,jsx,tsx}'
   ],
   presets: [],
-  darkMode: 'media', // or 'class'
+  darkMode: 'class', // or 'media'
   theme: {
     screens: {
       sm: '640px',
