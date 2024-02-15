b502cde8f90c4df343fc742fd85184688417e7f6
Cadiducho <cadiducho@gmail.com>
2024-02-15 20:27:09 +0100

    functional create season

--- src/pages/admin/seasons/CreateSeason.vue
+++ src/pages/admin/seasons/CreateSeason.vue
@@ -1,125 +1,72 @@
 <template>
     <div id="createSeason" class="box">
-        <PrognoPageTitle class="mb-5" name="Crear temporada" />
+        <PrognoPageTitle class="mb-5" name="Crear temporada"/>
 
-        <o-steps v-model="activeStep">
-            <o-step-item step="1" label="Datos de la temporada">
-                <h2 class="title">Datos de la temporada</h2>
+        <PInput label="Nombre de la temporada" name="name" v-model="createdSeason.name" />
+        <PInput label="Número de eventos totales" name="totalEvents" type="number" v-model="createdSeason.totalEvents" />
 
-                <o-field label="Nombre de la temporada">
-                    <o-input v-model="createdSeason.name" name="name" expanded lazy></o-input>
-                </o-field>
+        <o-select v-model="createdSeason.competitionId" placeholder="Selecciona una competición" expanded>
+            <option
+                v-for="comp in competitions"
+                :value="comp.id"
+                :key="comp.id">
+                {{ comp.name }} ({{ comp.code }}) - {{ comp.fullname }}
+            </option>
+        </o-select>
 
-                <o-field label="Número de eventos">
-                    <o-input v-model="createdSeason.totalEvents" name="longitude" expanded lazy type="number" step="any"></o-input>
-                </o-field>
-
-                <o-field label="Competición">
-                    <o-select v-model:class="createdSeason.competition" placeholder="Selecciona una competición" expanded>
-                        <option
-                            v-for="comp in competitions"
-                            :value="comp"
-                            :key="comp.id">
-                            {{ comp.name }} ({{ comp.code }}) - {{ comp.fullname }}
-                        </option>
-                    </o-select>
-                </o-field>
-
-                <hr/>
-
-            </o-step-item>
-
-            <o-step-item step="2" label="Finalizar">
-                <h2 class="title">Finalizar</h2>
-
-                <AlertInvalidData :object="createdSeason.name" message="No has introducido nombre para esta temporada"/>
-                <AlertInvalidData :object="createdSeason.totalEvents" message="No has introducido número de eventos para esta temporada"/>
-                <AlertInvalidData :object="createdSeason.competition" message="No has introducido competición para esta temporada"/>
-
-                <div class="notification has-background-primary">
-                    Revisa los datos, se va a crear la siguiente temporada
-                </div>
-
-                <div class="content">
-                    <p class="card-text"><b>Nombre de la temporada: </b>{{ createdSeason.name }}</p>
-                    <p class="card-text"><b>Eventos de la temporada: </b>{{ createdSeason.totalEvents }}</p>
-                    <p class="card-text" v-if="createdSeason.competition"><b>Competición: </b>{{ createdSeason.competition.name }}</p>
-                </div>
-
-                <hr/>
-                <o-field>
-                    <p class="control">
-                        <o-button :disabled="!isDataOk()" label="Crear temporada" @click="registerSeason()" variant="primary"/>
-                    </p>
-                </o-field>
-            </o-step-item>
-        </o-steps>
+        <PButton class="mt-4" :disabled="!isDataOk()" @click="registerSeason" type="solid" label="Crear temporada" />
     </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import PrognoPageTitle from "@/components/lib/PrognoPageTitle.vue";
 import {competitionService, notificationService, seasonService} from "@/_services";
-import AlertInvalidData from "@/components/lib/AlertInvalidData.vue";
-import AlertNoPermission from "@/components/lib/AlertNoPermission.vue";
-import {Season} from "@/types/Season";
 import {Competition} from "@/types/Competition";
 
-import {defineComponent} from "vue";
-import {useAuthStore} from "@/store/authStore";
+import {onMounted, reactive, ref} from "vue";
+import PButton from "@/components/lib/forms/PButton.vue";
+import PInput from "@/components/lib/forms/PInput.vue";
+import {useRouter} from "vue-router";
 
-export default defineComponent({
-    name: "CreateSeason",
-    components: {
-        AlertNoPermission,
-        AlertInvalidData,
-        PrognoPageTitle,
-    },
-    setup() {
-        const authStore = useAuthStore();
+const router = useRouter();
 
-        const currentUser = authStore.loggedUser;
-        return { currentUser };
-    },
-    data() {
-        return {
-            activeStep: 0,
-            competitions: new Array<Competition>(),
-            createdSeason: {
-                id: undefined!,
-                competition: undefined!,
-                name: undefined!,
-                totalEvents: undefined!
-            } as Season
-        }
-    },
-    mounted() {
-        competitionService.getCompetitionList().then((list) => {
-            this.competitions = [];
-            this.competitions.push(...list);
-        })
-    },
-    methods: {
-        isDataOk(): boolean {
-            return !(this.createdSeason.id == undefined && this.createdSeason.name == undefined && this.createdSeason.competition == undefined)
-        },
-        registerSeason(): void {
-            let rawSeason = {
-                name: this.createdSeason.name,
-                totalEvents: this.createdSeason.totalEvents,
-                competition: this.createdSeason.competition.name,
-            }
-
-            seasonService.createSeason(rawSeason).then((result) => {
-                notificationService.showNotification("Se ha registrado correctamente la temporada `" + result.name + "`");
-
-                this.$router.push({
-                    name: 'adminSeasons'
-                })
-            }).catch((error) => {
-                notificationService.showNotification(error.message, "error");
-            });
-        }
-    },
+const createdSeason = reactive({
+    name: '',
+    totalEvents: 0,
+    competitionId: 0
 });
+const competitions = ref<Competition[]>([]);
+
+onMounted(() => {
+    competitionService.getCompetitionList().then((list) => {
+        competitions.value = [];
+        competitions.value.push(...list);
+    })
+});
+
+const optionsName = (option: Competition) => {
+        return `${option.name} ${option.code} - ${ option.fullname }`
+}
+
+const isDataOk = (): boolean => {
+    return ((createdSeason.name.length > 0) && (createdSeason.totalEvents > 0) && (createdSeason.competitionId > 0))
+}
+const registerSeason = async () =>  {
+    let rawSeason = {
+        name: createdSeason.name,
+        totalEvents: createdSeason.totalEvents,
+        competition: createdSeason.competitionId
+    }
+
+    try {
+        const result = await seasonService.createSeason(rawSeason);
+        notificationService.showNotification("Se ha registrado correctamente la temporada `" + result.name + "`");
+
+        router.push({
+            name: 'adminSeasons'
+        })
+    } catch (error: any) {
+        notificationService.showNotification(error.message, "error");
+    }
+}
 </script>
