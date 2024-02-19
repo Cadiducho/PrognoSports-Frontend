8ed913f9ff19547640be233d30bde5b336caaf37
Cadiducho <cadiducho@gmail.com>
2024-02-19 21:16:05 +0100

    Create PSwitch
    Closes #100

---
+++ src/components/lib/forms/PSwitch.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="mb-3">
+        <label class="inline-flex items-center cursor-pointer">
+            <input type="checkbox" class="sr-only peer"
+                   v-model="model"
+                   id="switch"
+                   :checked="model"
+                   @change="$emit('onSwitch', $event.target.checked)">
+            <div
+                class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800
+                rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white
+                after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full
+                after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
+            </div>
+            <span v-if="props.label"
+                  class="ms-3 text-gray-700 font-bold dark:text-gray-300">
+                {{ props.label }}
+            </span>
+        </label>
+    </div>
+</template>
+
+<script setup lang="ts">
+export interface Props {
+    label: string;
+}
+const props = withDefaults(defineProps<Props>(), {
+    label: ''
+});
+const model = defineModel({type: Boolean});
+
+const emit = defineEmits<{
+    onSwitch: [value: boolean]
+}>();
+</script>
