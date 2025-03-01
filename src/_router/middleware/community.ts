4c41f1d6f294f12f0697f3c85643e61c2125cb50
Cadiducho <cadiducho@gmail.com>
2025-03-01 14:44:55 +0100

    remove logs

--- src/_router/middleware/community.ts
+++ src/_router/middleware/community.ts
@@ -5,20 +5,16 @@ import {useCommunityStore} from "@/store/communityStore";
 export default function checkCommunity(to: RouteLocationNormalized, from: RouteLocationNormalized, next: NavigationGuardNext) {
   const communityStore = useCommunityStore();
   if (to.matched.some(record => record.meta.requiresCommunity)) {
-    console.log('la ruta requiere comunidad', to)
     // Si no tiene comunidad pero la ruta lo requiere, mandar a /communities
     const hasCommunity = communityStore.thereIsCurrentCommunity;
     if (!hasCommunity) {
-      console.log('el usuario no tiene comunidad')
       next({
         path: '/communities'
       });
     } else {
-      console.log('el usuario tiene comunidad')
       next();
     }
   } else {
-    console.log('la ruta no requiere comunidad', to)
     next();
   }
 }
